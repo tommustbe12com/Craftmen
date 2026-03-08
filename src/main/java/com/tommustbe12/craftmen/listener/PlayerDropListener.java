9a2d9fe9dbab1dfca69e28b061df72e3424a35ed
tommustbe12com <155601372+tommustbe12com@users.noreply.github.com>
2026-03-07 21:33:49 -0800

    no dropping

---
+++ src/main/java/com/tommustbe12/craftmen/listener/PlayerDropListener.java
@@ -0,0 +1,22 @@
+package com.tommustbe12.craftmen.listener;
+
+import com.tommustbe12.craftmen.Craftmen;
+import com.tommustbe12.craftmen.profile.PlayerState;
+import com.tommustbe12.craftmen.profile.Profile;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerDropItemEvent;
+
+public class PlayerDropListener implements Listener {
+
+    @EventHandler
+    public void onPlayerDrop(PlayerDropItemEvent event) {
+        Profile profile = Craftmen.get().getProfileManager().getProfile(event.getPlayer());
+
+        if (profile == null) return;
+
+        if (profile.getState() != PlayerState.IN_MATCH) {
+            event.setCancelled(true);
+        }
+    }
+}
