cb56d6d8f966b23d190cbbf0c5ca53782caf7ff7
tommustbe12com <155601372+tommustbe12com@users.noreply.github.com>
2026-03-29 22:47:28 -0700

    spectating message shown to the players playing the match

--- src/main/java/com/tommustbe12/craftmen/command/SpectateCommand.java
+++ src/main/java/com/tommustbe12/craftmen/command/SpectateCommand.java
@@ -87,6 +87,9 @@ public class SpectateCommand implements CommandExecutor {
 
         player.sendMessage("§aNow spectating §f" + target.getName());
 
+        match.getP1().sendMessage(player.getName() + " is now spectating.");
+        match.getP1().sendMessage(player.getName() + " is now spectating.");
+
         return true;
     }
 
